27bcaf4ef6e5a9bebd6f7074eb6f5637ac7552cf
LucasCoder1 <lucas.developer.dm@gmail.com>
2021-07-15 21:56:20 +0200

    update

--- server.js
+++ server.js
@@ -63,3 +63,8 @@ server.start().then(function () {
     );
   });
 });
+
+app.use(express.static(ppath.join(__dirname, "client", "build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
